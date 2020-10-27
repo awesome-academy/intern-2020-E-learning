de06d66ef284962debf45e7c90a0849ead252bb1
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-10-27 14:57:49 +0700

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
 class User < ApplicationRecord
-  has_one :user_details, dependent: :delete_all
-  has_many :user_courses, dependent: :delete_all
+  USER_PARAMS = %i(name email password password_confirmation).freeze
+
+  has_one :user_details, dependent: :destroy
+  has_many :user_courses, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :instructor_courses, dependent: :destroy
+  has_many :course, through: :instructor_courses, dependent: :destroy
   has_many :courses, through: :user_courses, dependent: :destroy
+
+  validates :email, presence: true,
+    length: {maximum: Settings.email.max_length},
+    format: {with: URI::MailTo::EMAIL_REGEXP},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.password.min_length},
+    allow_nil: true
+
+  before_save :downcase_email
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#index"
+
+    get "/signup", to: "users#new", as: "signup"
+
+    resources :users
   end
 end
