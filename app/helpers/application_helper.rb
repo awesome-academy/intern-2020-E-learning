4ff86320dee4dd2ea311b44a75144a26459fface
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-10-29 11:18:34 +0700

    Add Course

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,1 +1,6 @@
-module ApplicationHelper; end
+module ApplicationHelper
+  def page_index params_page, index, per_page
+    params_page ||= 1
+    (params_page.to_i - 1) * per_page.to_i + index.to_i + 1
+  end
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
+  COURSE_PARAMS = %i(name description status estimate_time).freeze
+  enum status: {unactive: 0, active: 1, on_progress: 2}
+
   has_many :categories, through: :course_categories, dependent: :destroy
   has_many :user_courses, dependent: :destroy
   has_many :instructor_courses, dependent: :destroy
@@ -7,4 +10,6 @@ class Course < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :courses, through: :user_courses, dependent: :destroy
   has_many :categories, through: :course_categories, dependent: :destroy
+
+  scope :order_by_created_at, ->{order created_at: :desc}
 end
