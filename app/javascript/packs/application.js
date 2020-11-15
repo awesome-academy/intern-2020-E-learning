13e71aecd7bedcd625a0dba8185c9212f58585de
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2020-11-15 15:41:34 +0700

    Add toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 const jquery = require('jquery')
+const toastr = require('toastr')
 
 require('./course')
 require('@rails/ujs').start()
@@ -9,6 +10,12 @@ require('bootstrap/dist/js/bootstrap')
 require('jquery')
 require('jquery-ui')
 require('@nathanvda/cocoon')
+
+toastr.options = {
+  "positionClass": "toast-bottom-right"
+};
+
+global.toastr = toastr;
 
 require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.css/ );
 require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.theme\.css/ );
